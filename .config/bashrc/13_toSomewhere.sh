83b4110fca3fbc71697d634f9742073eea1a82d0
Gl00ria <glooria.bbh@gmail.com>
2022-10-19 20:21:51 +0300

    FirstCommit

---
+++ .config/bashrc/13_toSomewhere.sh
@@ -0,0 +1,47 @@
+#_______________________________________________________________________________________#
+#                                                                                       #
+#              ██████╗ ██╗      ██████╗  ██████╗ ██████╗ ██╗ █████╗                     #
+#             ██╔════╝ ██║     ██╔═████╗██╔═████╗██╔══██╗██║██╔══██╗                    #
+#             ██║  ███╗██║     ██║██╔██║██║██╔██║██████╔╝██║███████║                    #
+#             ██║   ██║██║     ████╔╝██║████╔╝██║██╔══██╗██║██╔══██║                    #
+#             ╚██████╔╝███████╗╚██████╔╝╚██████╔╝██║  ██║██║██║  ██║                    #
+#              ╚═════╝ ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝                    #
+#_______________________________________________________________________________________#
+#                                                                                       #
+#                                                                                       #
+#             :   Configured by  :  Gl00ria @gl00ria                                    #
+#             :     GitHub       :  https://github.com/gl00ria                          #
+#                                                                                       #
+#             :     @file        :  13_toSomewhere.sh                                   #
+#             :     @folder      :  ~/.config/bash/                                     #
+#             :     @brief       :  aliases to jump to config folders, web-page &&      #
+#                                            change current shell                       #
+#             :     @requires    :  bashShell, zshShell, fishShell, xdg-utils,          #
+#                                        zsh-web_search plugin                          #
+#             :     @optional    :  none                                                #
+#             :     @Note        :  none                                                #
+#_______________________________________________________________________________________#
+#                                                                                       #
+#             :    Created on   :   26, Sep, 2022                                       #
+#             :   Last Updated  :   26, Sep, 2022                                       #
+# Update History :                                                                      #
+#_______________________________________________________________________________________#
+#                                                                                       #
+
+## To configs
+alias cfg.bash="cd $HOME/.config/bashrc"
+alias cfg.zsh="cd $HOME/.config/zshrc"
+alias cfg.zhs="cd $HOME/.config/zshrc"
+alias cfg.fish="cd $HOME/.config/fish"
+alias cfg.starship="cd $HOME/.config/starship"
+alias cfg.nvim="cd $HOME/.config/nvim"
+alias cfg.gloorifiles="cd $HOME/gloorifiles"
+
+## Changing shells
+alias tobash="sudo chsh $USER -s /bin/bash && echo 'Now log out.'"
+alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Now log out.'"
+alias tofish="sudo chsh $USER -s /bin/fish && echo 'Now log out.'"
+
+## To the INETRNET
+alias web-search-archwiki="xdg-open https://wiki.archlinux.org/&"
+alias web-search="xdg-open" # search the link provided by the user
---
+++ .config/fish/conf.d/02_pacman.fish
@@ -0,0 +1,62 @@
+#_______________________________________________________________________________________#
+#                                                                                       #
+#              ██████╗ ██╗      ██████╗  ██████╗ ██████╗ ██╗ █████╗                     #
+#             ██╔════╝ ██║     ██╔═████╗██╔═████╗██╔══██╗██║██╔══██╗                    #
+#             ██║  ███╗██║     ██║██╔██║██║██╔██║██████╔╝██║███████║                    #
+#             ██║   ██║██║     ████╔╝██║████╔╝██║██╔══██╗██║██╔══██║                    #
+#             ╚██████╔╝███████╗╚██████╔╝╚██████╔╝██║  ██║██║██║  ██║                    #
+#              ╚═════╝ ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝                    #
+#_______________________________________________________________________________________#
+#                                                                                       #
+#                                                                                       #
+#             :   Configured by  :  Gl00ria @gl00ria                                    #
+#             :     GitHub       :  https://github.com/gl00ria                          #
+#                                                                                       #
+#             :     @file        :  02_pacman.fish                                      #
+#             :     @folder      :  ~/.config/fish/conf.d/                              #
+#             :     @brief       :  aliases for the pkg manager "PACMAN"                #
+#             :     @requires    :  zshShell, pacman, fzf                               #
+#             :     @optional    :  none                                                #
+#             :     @Note        :  none                                                #
+#_______________________________________________________________________________________#
+#                                                                                       #
+#             :    Created on   :   26, Sep, 2022                                       #
+#             :   Last Updated  :   26, Sep, 2022                                       #
+# Update History :                                                                      #
+#_______________________________________________________________________________________#
+#                                                                                       #
+
+alias sync="sudo pacman -Syy"                                    							 ## Refresh all pkgs lists after updating the mirrorlist
+alias pacupgrade="sudo pacman -Syu"                                            ## Upgrade
+
+## Arch's nuclear
+alias upgrade-all="sudo pacman -Syy && sudo pacman -Syu && yay -Sua --noconfirm"
+
+## Install
+alias pacinstall="sudo pacman -S"                                              ## Install pkgs from the repo using "PACMAN"
+alias paclocal-install="sudo pacman -U"                                        ## Install pkgs from a local file using "PACMAN"
+alias pacinstall-deps="sudo pacman -S --asdeps"                                ## Install pkgs as dependencies of another pkg using "PACMAN"
+
+## Info
+alias pacpkg-info="sudo pacman -Qi"                                            ## Query installed pkgs and their files
+alias pacpkg-list="sudo pacman -Ql"                                            ## List files in a pkg
+alias pacsearch-local="sudo pacman -Qs"                                         ## Search installed pkgs
+
+## Search & install pkgs in pacman's repo & mark the installed pkgs with *
+function pacsearch
+  pacman -Sl | awk '{print $2($4=="" ? "" : " *")}' | fzf --multi --ansi --preview 'pacman -Si {1}' --reverse | xargs -ro sudo pacman -S
+end
+
+alias pkgtree="pactree"                                                      ## Find out the dependencies for a pkg
+
+## Clean Up
+alias clr-cache="sudo pacman -Sc --noconfirm && yay -Sc --noconfirm"         ## Clear old & unused chaches & pkgs
+alias clr-allchace="sudo pacman -Scc --noconfirm && yay -Scc --noconfirm"    ## Clear all files from the cache
+alias pacremove="pacman -Qq | fzf --multi --preview 'pacman -Qi {1}' | xargs -ro sudo pacman -R"                 ## Remove a pkg, keeps its settings & dependencies
+alias pacremove-with-settings="pacman -Qq | fzf --multi --preview 'pacman -Qi {1}' | xargs -ro sudo pacman -Rns" ## Remove a pkg, including its settings & dependencies
+alias pacuseless="sudo pacman -Rs $(pacman -Qtdq)"                             ## Remove unneeded pkgs
+
+alias unlock="sudo rm /var/lib/pacman/db.lck"                                ## Remove pacman lock
+
+## get fastest mirrors
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
